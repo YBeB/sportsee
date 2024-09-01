3de0b2f8f6feb537c4db6c2bc93b8b239879f70f
ybeb <younes.bouchi@gmail.com>
2024-09-01 22:31:57 +0200

    majmaj

--- src/components/AverageSessionChart/AverageSessionChart.jsx
+++ src/components/AverageSessionChart/AverageSessionChart.jsx
@@ -25,26 +25,35 @@ CustomTooltip.propTypes = {
 };
 
 function AverageSessionsChart  ({ sessions })  {
+
     return (
-        <div className="chart-section" style={{ backgroundColor: '#ff0101', borderRadius: '5px' }}>
-        <h2 style={{ color: '#fff' }}>Durée moyenne des sessions</h2>
+        
+        <div className="chart-section" style={{background: "linear-gradient(to right, #ff0000 70%, #e60000 70%, #e60000 100%)", borderRadius: '5px' ,  }}>
+        <h2 style={{ color: '#fff',opacity:'0.5' , fontSize:'15px',width:'120px',display:'flex',justifyContent:'center' }}>Durée moyenne des sessions</h2>
         <ResponsiveContainer width="100%" height="60%">
             <LineChart 
                 data={sessions} 
                 margin={{ top: 56, right: 0, left: 0, bottom: 9 }} // Augmenter la marge en bas pour remonter l'axe X
             >
+                    <defs>
+    <linearGradient id="lineGradient" x1="0" y1="0" x2="1" y2="0">
+        <stop offset="0%" stopColor="#fff" stopOpacity={0.1} />
+        <stop offset="100%" stopColor="#fff" stopOpacity={1} />
+    </linearGradient>
+</defs>
                 <XAxis 
                     dataKey="day" 
                     stroke="#fff" 
-
-       
+            opacity={0.5}
+            axisLine={false}
                 />
                 <YAxis hide={true} />
                 <Tooltip content={<CustomTooltip />} />
                 <Line
                     type="monotone"
                     dataKey="sessionLength"
-                    stroke="#fff"
+                    
+                stroke="url(#lineGradient)"
                     strokeWidth={3}
                     dot={false}
                     activeDot={{ r: 8, strokeWidth: 0 }}
